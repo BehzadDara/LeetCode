3890e5ae2261a8232ff6fadb60cc7d90bf944403
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 19:53:15 +0330

    EarliestPossibleDayOfFullBloom

---
+++ EarliestPossibleDayOfFullBloom/Problem.cs
@@ -0,0 +1,25 @@
+﻿namespace EarliestPossibleDayOfFullBloom;
+
+public static class Problem
+{
+    public static int Solve(int[] plantTime, int[] growTime)
+    {
+        var result = 0;
+
+        var list = new List<Tuple<int, int>>();
+        for (int i = 0; i < plantTime.Length; i++)
+        {
+            list.Add(Tuple.Create(plantTime[i], growTime[i]));
+        }
+        list = list.OrderByDescending(x => x.Item2).ToList();
+
+        var day = 0;
+        foreach (var item in list)
+        {
+            day += item.Item1;
+            result = Math.Max(result, day + item.Item2);
+        }
+
+        return result;
+    }
+}
---
+++ EarliestPossibleDayOfFullBloom/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://leetcode.com/problems/earliest-possible-day-of-full-bloom/
+
+using EarliestPossibleDayOfFullBloom;
+using Shared.Helpers;
+
+var testCase1 = new int[] { 1, 2, 3, 2 };
+var testCase2 = new int[] { 2, 1, 2, 1 };
+
+var output = Problem.Solve(testCase1, testCase2);
+
+output.Print();
