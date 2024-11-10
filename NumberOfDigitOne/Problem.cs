62350b126ed32a0bf358adb92b387a46fd0f799b
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 19:46:55 +0330

    Number-of-Digit-One

---
+++ NumberOfDigitOne/Problem.cs
@@ -0,0 +1,31 @@
+﻿namespace NumberOfDigitOne;
+
+public static class Problem
+{
+    public static int Solve(int n)
+    {
+        var result = 0;
+
+        for (var i = 1; i <= n; i *= 10)
+        {
+            var before = n / i;
+            var current = before % 10;
+
+            if (current == 0)
+            {
+                result += (before / 10) * i;
+            }
+            else if (current == 1)
+            {
+                var after = n % i;
+                result += (before / 10) * i + (after + 1);
+            }
+            else
+            {
+                result += (before / 10 + 1) * i;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ NumberOfDigitOne/Program.cs
@@ -0,0 +1,10 @@
+﻿// https://leetcode.com/problems/number-of-digit-one/
+
+using NumberOfDigitOne;
+using Shared.Helpers;
+
+var testCase = 192;
+
+var output = Problem.Solve(testCase);
+
+output.Print();
