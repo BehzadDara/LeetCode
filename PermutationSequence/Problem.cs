38040172b2b9df36d9f5868c330a9a607cef0999
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 19:58:20 +0330

    PermutationSequence

---
+++ PermutationSequence/Problem.cs
@@ -0,0 +1,43 @@
+﻿namespace PermutationSequence;
+
+public static class Problem
+{
+    public static string Solve(int n, int k)
+    {
+        if (n == 1)
+            return "1";
+
+        var nextBase = Factorial(n - 1);
+
+        var digit = Math.Ceiling((double)k / nextBase);
+        if (digit == 0)
+            digit = n;
+
+        var result = digit.ToString();
+
+        var tmpResult = Solve(n - 1, k % nextBase);
+        foreach (char c in tmpResult)
+        {
+            var tmpDigit = int.Parse(c.ToString());
+
+            if (tmpDigit >= digit)
+                result += tmpDigit + 1;
+            else
+                result += tmpDigit;
+        }
+
+        return result;
+    }
+
+    static int Factorial(int n)
+    {
+        var result = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            result *= i;
+        }
+
+        return result;
+    }
+}
---
+++ PermutationSequence/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://leetcode.com/problems/permutation-sequence/
+
+using PermutationSequence;
+using Shared.Helpers;
+
+var testCase1 = 4;
+var testCase2 = 15;
+
+var output = Problem.Solve(testCase1, testCase2);
+
+output.Print();
