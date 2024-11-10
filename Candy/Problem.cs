d7c1d845244fcc42ccb7a572bc74d1d026ab2229
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 20:33:43 +0330

    Candy

---
+++ Candy/Problem.cs
@@ -0,0 +1,39 @@
+﻿namespace Candy;
+
+public static class Problem
+{
+    public static int Solve(int[] ratings)
+    {
+        var result = new int[ratings.Length];
+        var value = 1;
+
+        for (int i = 0; i < ratings.Length - 1; i++)
+        {
+            result[i] = value;
+            if (ratings[i] < ratings[i + 1])
+            {
+                value++;
+            }
+            else if (ratings[i] == ratings[i + 1])
+            {
+                value = 1;
+            }
+            else
+            {
+                value = 1;
+
+                var j = i;
+                var tmpValue = 1;
+                while (j >= 0 && ratings[j] > ratings[j + 1] && result[j] == tmpValue)
+                {
+                    result[j]++;
+                    tmpValue++;
+                    j--;
+                }
+            }
+        }
+        result[ratings.Length - 1] = value;
+
+        return result.Sum();
+    }
+}
---
+++ Candy/Program.cs
@@ -0,0 +1,10 @@
+﻿// https://leetcode.com/problems/candy/
+
+using Candy;
+using Shared.Helpers;
+
+var testCase = new int[] { 1, 0, 2 };
+
+var output = Problem.Solve(testCase);
+
+output.Print();
