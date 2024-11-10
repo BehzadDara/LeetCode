34ec8fffe6e297c232011d79d323c1643a60f05c
behzad dara <Behzad.dara.99@gmail.com>
2024-11-10 19:06:14 +0330

    ZigzagConversion

---
+++ ZigzagConversion/Problem.cs
@@ -0,0 +1,42 @@
+﻿using System.Text;
+
+namespace ZigzagConversion;
+
+public static class Problem
+{
+    public static string Solve(string s, int numRows)
+    {
+        if (numRows == 1)
+            return s;
+
+        var stringBuilders = new StringBuilder[numRows];
+
+        for (int i = 0; i < numRows; i++)
+        {
+            stringBuilders[i] = new StringBuilder();
+        }
+
+        var index = 0;
+        while (index < s.Length)
+        {
+            for (int i = 0; i < numRows; i++)
+            {
+                if (index + i < s.Length)
+                {
+                    stringBuilders[i].Append(s[index + i]);
+                }
+            }
+            for (int i = numRows; i < 2 * (numRows - 1); i++)
+            {
+                if (index + i < s.Length)
+                {
+                    stringBuilders[2 * (numRows - 1) - i].Append(s[index + i]);
+                }
+            }
+
+            index += 2 * (numRows - 1);
+        }
+
+        return string.Join("", stringBuilders.Select(x => x));
+    }
+}
---
+++ ZigzagConversion/Program.cs
@@ -0,0 +1,11 @@
+﻿// https://leetcode.com/problems/zigzag-conversion/
+
+using Shared.Helpers;
+using ZigzagConversion;
+
+var testCase = "PAYPALISHIRING";
+var numRows = 3;
+
+var output = Problem.Solve(testCase, numRows);
+
+output.Print();
